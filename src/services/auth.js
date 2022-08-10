50851571779a9fc34c5ab433366efd0ecfa84d80
H. Indiana Holdsworth <88062154+H-Indiana-Holdsworth@users.noreply.github.com>
2022-08-10 16:43:21 -0700

    Add token retrieval and local storage delete logic

--- src/services/auth.js
+++ src/services/auth.js
@@ -57,3 +57,46 @@ export async function buildAuthorizeEndpointAndRedirect() {
   // Redirect to authentication server's login page
   window.location = endpoint;
 }
+
+// Retrieve token
+
+async function getToken(verifier) {
+  const host = 'https://login-sandbox.procore.com/oauth/token';
+  const clientId =
+    '7b18a910f1b9cce5d80479e0d9f260f3bc0293158603e3f2dbdb559ed8314140';
+  const redirectUri = `http://localhost:4567/callback`;
+  const grantType = 'authorization_code';
+
+  // Get code from query params
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get('code');
+
+  // Build params to send to token endpoint
+  const params = `client_id=${clientId}&
+      grant_type=${grantType}&
+      code_verifier=${verifier}&
+      redirect_uri=${redirectUri}&
+      code=${code}`;
+
+  // Make a POST request
+  try {
+    const response = await fetch(host, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: params,
+    });
+    const data = await response.json();
+
+    // Token
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// Delete verifier token from local storage
+
+const response = await getToken(localStorage.getItem('verifier'));
+localStorage.removeItem('verifier');
